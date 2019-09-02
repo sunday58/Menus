fb7bdaae4db52d65417bdd9bc2fb1049fcb60008
Sunday David <sundaydavid989@gmail.com>
2019-09-02 15:31:44 +0100

    options menu created

--- app/src/main/java/com/example/android/menus/MainActivity.java
+++ app/src/main/java/com/example/android/menus/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.android.menus;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options_menu, menu);
+        return true;
     }
 }
