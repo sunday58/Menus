b1be9724514b224818c4719bd0ccdf9c491232b3
Sunday David <sundaydavid989@gmail.com>
2019-09-03 09:46:07 +0100

    options menu

--- app/src/main/java/com/example/android/menus/MainActivity.java
+++ app/src/main/java/com/example/android/menus/MainActivity.java
@@ -3,7 +3,10 @@ package com.example.android.menus;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,5 +22,25 @@ public class MainActivity extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.options_menu, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        //handling options menu item click
+        switch (item.getItemId()){
+            case R.id.menuItem1:
+                Toast.makeText(this, "Menu Item 1 Clicked", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.menuItem2:
+                Toast.makeText(this, "Menu Item 2 Clicked", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.menuItem3:
+                Toast.makeText(this, "Menu Item 3 Clicked", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
     }
 }
